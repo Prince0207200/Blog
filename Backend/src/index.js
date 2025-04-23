104aae4b8c075c4f2c517e4b95f2546ebe09c946
Prince <princegupta1455@gmail.com>
2025-04-23 22:13:06 +0530

    commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,7 +14,7 @@ connnectDB()
     })
 })
 .then(()=>{
-    const port=process.env.PORT || 8000
+    const port=process.env.PORT || 8001
     app.listen(port,()=>{
         console.log(`Server is running at port: ${port}`);
         
