8040daf3eb11a77ec52c4f2523910f19c3c5f11b
Prince Gupta <121662994+Prince0207200@users.noreply.github.com>
2025-04-30 06:39:12 +0530

    Update Home.jsx
    running test added

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -23,7 +23,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Your post will be added after
+                                Your post will be added after running test
                             </h1>
                         </div>
                     </div>
