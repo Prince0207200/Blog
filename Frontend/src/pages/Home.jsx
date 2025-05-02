00713b5693b5a8d365cc6bff681c826cf6ea1058
Prince Gupta <121662994+Prince0207200@users.noreply.github.com>
2025-05-02 12:29:34 +0530

    Update Home.jsx
    new changes

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -23,7 +23,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Your post will be added after running testing
+                                Your post will be added here Making new Changes
                             </h1>
                         </div>
                     </div>
